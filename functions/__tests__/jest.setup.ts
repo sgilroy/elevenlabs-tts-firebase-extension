64b252fa4def4494b7569063e755dabc62470a4e
Scott Gilroy <scottjgilroy@gmail.com>
2024-05-07 15:58:42 -0400

    Rename the extension to firestore-elevenlabs-tts

--- functions/__tests__/jest.setup.ts
+++ functions/__tests__/jest.setup.ts
@@ -3,13 +3,10 @@ const path = require("path");
 
 (async function () {
   dotenv.config({
-    path: path.resolve(
-      __dirname,
-      "../../_emulator/extensions/elevenlabs-tts-firebase-extension.env.local"
-    ),
+    path: path.resolve(__dirname, "../../_emulator/extensions/firestore-elevenlabs-tts.env.local"),
   });
 
-  process.env.EXT_INSTANCE_ID = "elevenlabs-tts-firebase-extension";
+  process.env.EXT_INSTANCE_ID = "firestore-elevenlabs-tts";
   process.env.GOOGLE_CLOUD_PROJECT = "demo-gcp";
   process.env.GCLOUD_PROJECT = "demo-gcp";
   process.env.PROJECT_ID = "demo-gcp";
